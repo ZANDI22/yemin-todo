d4b60ece8168441373bd1b38d7b0646566108cc3
ddmkim94 <ddmkim94@gmail.com>
2022-11-22 15:09:13 +0900

    :sparkles: Feat: API Call
    Srping Boot, React 연결
    package.json에 proxy 추가

---
+++ src/main/java/com/yemin/todo/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.yemin.todo.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String home() {
+        return "동민+예현";
+    }
+}
